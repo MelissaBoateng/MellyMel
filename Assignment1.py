e5059ebdf52e32c3fe469738a587ae5de00f036b
MelissaBoateng <67985151+MelissaBoateng@users.noreply.github.com>
2020-07-15 23:32:55 +0200

    Delete Assignment1.py

--- Assignment1.py
+++
@@ -1,8 +0,0 @@
-'''Question 1: DotProduct '''
-from random import sample
-import math
-
-def Dotproduct(N,ListA,ListB):
-    Dotproduct=[x*y for x,y in zip(ListA,ListB)]
-    DP= sum(Dotproduct)
-    print(DP)
