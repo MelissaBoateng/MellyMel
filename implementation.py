78e9caffa35469dc1640a70fdedab069f44fc5c2
MelissaBoateng <67985151+MelissaBoateng@users.noreply.github.com>
2020-07-16 10:53:20 +0200

    Update implementation.py

--- implementation.py
+++ implementation.py
@@ -1,4 +1,5 @@
 from dot import*
+''' Testing using N=10 with random variables, The sample class of the random library allows the function get randomized sample of size N'''
 
 N = 10
 ListA= sample(range(1,1000),N)
